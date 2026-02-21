4498aac2fdadaf0375e66fb5ca4379dd8f848822
smilee <mwanglewis6@gmail.com>
2026-02-22 02:15:19 +0300

    modelcont

--- modelling.py
+++ modelling.py
@@ -22,3 +22,17 @@ for c in df3.columns:
 
 y = df3.pop('Credit_score')
 x = df3
+
+# data resampling
+X_resampled, y_resampled = SMOTE(random_state=42).fit_resample(x,y)
+
+# split into train and testing data
+X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.3, random_state=42, shuffle=True, stratify=y_resampled)
+
+#Models
+
+models = {
+    'K_Nearest_Neighbour': KNN(n_neighbors=5, weights='distance', algorithm='ball_tree', leaf_size=30),
+    'Decision_Tree': DTC(criterion='entropy', max_depth=40, splitter='best', random_state=42, max_features='sqrt'),
+    'Random_forest': RFC(n_estimators=150, criterion='gini', max_features='sqrt', random_state=42, max_depth=30, class_weight='balanced_subsample')
+}
